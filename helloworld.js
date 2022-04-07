2feec8cdafe33c86b24409bfd5a6c2ce95404af3
Rio Landa <Riolndn@gmail.com>
2022-04-07 19:15:33 +0100

    chore: fixed formating first lesson javascript

--- helloworld.js
+++ helloworld.js
@@ -5,41 +5,41 @@ console.log("Hello this is my third line");
 var myname = "rio";
 let lastname = "Landa";
 
-for (let i = 0; i <= 9; i = i+1){
-    console.log(myname)
+for (let i = 0; i <= 9; i = i + 1) {
+  console.log(myname);
 }
 
-//var = keyword for global variables 
+//var = keyword for global variables
 // let = keyword for local variables
 
 var number = 5;
 
-while (number < 15)
-{
-    console.log(number)
-    number = number + 1
-}
-console.log(number)
-if (number == 15)
-{
-    console.log("number is 15")
-}
-else if (number == 16)
-{
-    console.log("number is 16")
-}
-else 
-{
-    console.log("number is bigger than 16")
+while (number < 15) {
+  console.log(number);
+  number = number + 1;
 }
 
-function saygoodbye()
-{
-    console.log("this is my first javascript code")
+console.log(number);
+
+if (number == 15) {
+  console.log("number is 15");
+
+
+} else if (number == 16) {
+  console.log("number is 16");
+
+} else {
+  console.log("number is bigger than 16");
+} 
+
+function saygoodbye() {
+  console.log("this is my first javascript code");
 }
-saygoodbye()
+
+saygoodbye();
 //function is the keyword to declare a function
 // to use a function type name of the fucntion and use round brackets
 //if function doesnt have return you cant print it or put it in a variable
-var list = ["london","washington"]
-console.log(list[0])
+
+var list = ["london", "washington"];
+console.log(list[0]);
