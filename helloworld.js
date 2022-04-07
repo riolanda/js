c51a8eec67419f509fb1667c26f6b4c377270dfc
Rio Landa <Riolndn@gmail.com>
2022-04-07 19:07:09 +0100

    first lesson javascript

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,44 @@ console.log("Hello, World!");
 
 console.log("Hello this is javascript");
 console.log("Hello this is my third line");
+var myname = "rio";
+let lastname = "Landa";
+
+for (let i = 0; i <= 9; i = i+1){
+    console.log(myname)
+}
+
+//var = keyword for global variables 
+// let = keyword for local variables
+
+var number = 5;
+
+while (number < 15)
+{
+    console.log(number)
+    number = number + 1
+}
+console.log(number)
+if (number == 15)
+{
+    console.log("number is 15")
+}
+else if (number == 16)
+{
+    console.log("number is 16")
+}
+else 
+{
+    console.log("number is bigger than 16")
+}
+
+function saygoodbye()
+{
+    console.log("this is my first javascript code")
+}
+saygoodbye()
+//function is the keyword to declare a function
+// to use a function type name of the fucntion and use round brackets
+//if function doesnt have return you cant print it or put it in a variable
+var list = ["london","washington"]
+console.log(list[0])
